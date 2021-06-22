8206521a536e200edb06ad2e56877a35a0135b56
gusajr <gucho35.enp9@gmail.com>
2021-06-22 18:53:07 -0500

    22_06_21

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
-mkdir -p ~/.streamlit/echo "\
-[server]\n\
-headless = true\n\
-port = $PORT\n\
-enableCORS = false\n\
-\n\
-" > ~/.streamlit/config.toml
+mkdir -p ~/.streamlit/
+echo "[general]"  > ~/.streamlit/credentials.toml
+echo "email = \"gucho35.enp9@gmail.com\""  >> ~/.streamlit/credentials.toml
+echo "[server]"  > ~/.streamlit/config.toml 
+echo "headless = true"  >> ~/.streamlit/config.toml
+echo "port = $PORT"  >> ~/.streamlit/config.toml
+echo "enableCORS = true"  >> ~/.streamlit/config.toml
