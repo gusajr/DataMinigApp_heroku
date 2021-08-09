e984df2f46d3f99587db4e901241a24068d894bb
gusajr <gucho35.enp9@gmail.com>
2021-08-08 21:33:52 -0500

    08_08_2021

--- setup.sh
+++ setup.sh
@@ -2,8 +2,7 @@ mkdir -p ~/.streamlit/
 echo "[general]"  > ~/.streamlit/credentials.toml
 echo "email = \"gucho35.enp9@gmail.com\""  >> ~/.streamlit/credentials.toml
 echo "[theme]
-base='dark'
-primaryColor='purple'"
+base='dark'"
 echo "[server]"  > ~/.streamlit/config.toml 
 echo "headless = true"  >> ~/.streamlit/config.toml
 echo "port = $PORT"  >> ~/.streamlit/config.toml
